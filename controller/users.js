49694eda5cbbc253b240d98fc191338b2d7c1e48
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-03-28 11:38:55 +0100

    Clean code

--- controller/users.js
+++ controller/users.js
@@ -5,48 +5,47 @@ let users = []
 // GET ALL USERS
 export const getAllUsers = (req, res) => {
     res.send(users)   
+
    // Class : Utenti
     class Utenti {
         constructor(nome, cognome, email, id, codice){
-           //this.dati = {"Dati dell'utente": users, "codice": res.statusCode}
            this.nome = nome;
            this.cognome = cognome;
            this.email = email;
            this.id = id;
            this.codice = codice;
-            //this.codice = res.statusCode // print the status code as well as a *property* of this Class
         }
     }
-    // Istanc : singolo_utente
+
+    // Instance : singolo_utente
     users.forEach((user)=>{
 
         let utente_campione = new Utenti(user.nome, user.cognome, user.email, user.id, res.statusCode);
         for (const [key,value] of Object.entries(utente_campione)) {
             console.log(key, ":", value);
-            /* Output:
-
-            1) OLD OUTPUT BEFORE LAST COMMIT
-                dati : [
-                    {
-                        nome: 'Alex',
-                        cognome: 'Bartolini',
-                        email: 'alexbartolini@gmail.com'
-                        id: 'xxxxx'
-                        }
-                        ]
-                        codice : 200
-
-                2) NEW OUTPUT AFTER LAST COMMIT
-                {
-                    nome : "Alex",
-                    cognome : "Bartolini",
-                    email: "email",
-                    id: "string_id",
-                    codice : 200 (number: int data type)
-                }         
-            */
         } 
-    })
+
+        /* Output:
+        1) OLD OUTPUT BEFORE LAST COMMIT
+            dati : [
+                {
+                    nome: 'Alex',
+                    cognome: 'Bartolini',
+                    email: 'alexbartolini@gmail.com'
+                    id: 'xxxxx'
+                    }
+                    ]
+                    codice : 200
+            2) NEW OUTPUT AFTER LAST COMMIT
+            {
+                nome : "Alex",
+                cognome : "Bartolini",
+                email: "email",
+                id: "string_id",
+                codice : 200 (number: int data type)
+            }         
+        */
+    });
 }
 
 // GET USER BY ID
