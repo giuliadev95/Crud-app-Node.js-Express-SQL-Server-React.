0382f89e73ee3ebb706562834e5311821edd20d6
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-04-01 12:03:23 +0200

    Install React / Vite app as frontend

--- backend/index.js
+++ backend/index.js
@@ -12,27 +12,31 @@ app.use(express.static('public'));
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
-// '/homepage.html'
+// Everything goes on localhost:5000/users
+app.use('/users', usersRoutes);
+
+// homepage
 app.get('/', (req, res)=> {
     res.sendFile('homepage.html', { 
         root:__dirname + "/public"
-    })
+    });
 });
 
+// contatti page
 app.get( '/contatti', (req, res)=> {
     res.sendFile('contatti.html', {
         root:__dirname + "/public"
-    })
+    });
 });
-app.use('/users', usersRoutes);
 
-
+// Error page
 app.get('*', (req, res)=>{
     res.sendFile('404.html', {
         root:__dirname + "/public"
     });
-
 });
+
+// Server listen at port 5000
 app.listen(PORT, ()=> {
     console.log('I\'m listening');   
 });
